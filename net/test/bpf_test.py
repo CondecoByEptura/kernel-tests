870d69d163218898b642101863df0cf7b082fc73
Lorenzo Colitti <lorenzo@google.com>
2016-12-20 11:20:28 +0900

    WIP: Proof-of-concept eBPF test.
    Change-Id: I9d112ca637c0374b50c33c9bfafade4292ee0ad9
    
    baisc tests for eBPF maps
    
    Add some kernel net tests for evaluating eBPF maps. The tests verified
    the basic eBPF map syscalls such as create map, update element, lookup
    element and delete elements. It is prepared for further testcase
    development related to xt_eBPF modules.
    
    Test: follow the instructions in go/android-net-test to setup the kernel
    then run this test by enter the following command when under your kernel
    tree:
    [your android tree path]/kernel/tests/net/test/run_net_test.sh bpf_test.py
    
    Change-Id: I25b8877c58998e712569d7922b0531852357c6b8

---
+++ net/test/bpf_test.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+#
+# Copyright 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ctypes
+import unittest
+import struct
+
+import cstruct
+import net_test
+
+
+# TODO: figure out how to make this arch-dependent if we run these tests on non-X86
+__NR_bpf = 321
+
+# BPF syscall commands constants.
+BPF_MAP_CREATE = 0
+BPF_MAP_LOOKUP_ELEM = 1
+BPF_MAP_UPDATE_ELEM = 2
+BPF_MAP_DELETE_ELEM = 3
+BPF_MAP_GET_NEXT_KEY = 4
+BPF_PROG_LOAD = 5
+BPF_OBJ_PIN = 6
+BPF_OBJ_GET = 7
+
+# BPF map type constant.
+BPF_MAP_TYPE_UNSPEC = 0
+BPF_MAP_TYPE_HASH = 1
+BPF_MAP_TYPE_ARRAY = 2
+BPF_MAP_TYPE_PROG_ARRAY = 3
+BPF_MAP_TYPE_PERF_EVENT_ARRAY = 4
+
+# BPF program type constant.
+BPF_PROG_TYPE_UNSPEC = 0
+BPF_PROG_TYPE_SOCKET_FILTER = 1
+BPF_PROG_TYPE_KPROBE = 2
+BPF_PROG_TYPE_SCHED_CLS = 3
+BPF_PROG_TYPE_SCHED_ACT = 4
+
+# BPF attr struct
+BpfAttrCreate = cstruct.Struct("bpf_attr_create", "=IIII", "map_type key_size value_size max_entries")
+BpfAttrOperation = cstruct.Struct("bpf_attr_ops", "=QQQQ", "map_fd key_ptr value_ptr flags")
+BpfAttrProgLoad = cstruct.Struct("bpf_attr_prog_load", "=IIQQIIQI", "prog_type insn_cnt insns"
+                                 "license log_level log_size log_buf kern_version")
+
+libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
+HAVE_EBPF_SUPPORT = net_test.LINUX_VERSION >= (4, 4, 0)
+
+def CreateMap(map_type, key_size, value_size, max_entries):
+  attr = BpfAttrCreate((map_type, key_size, value_size, max_entries))
+  ret = libc.syscall(__NR_bpf, BPF_MAP_CREATE, attr.CPointer(), len(attr))
+  return ret
+
+def UpdateMap(map_fd, key, value, flags = 0):
+  _value = ctypes.c_uint32(value)
+  _key = ctypes.c_uint32(key)
+  value_ptr = ctypes.addressof(_value)
+  key_ptr = ctypes.addressof(_key)
+  attr = BpfAttrOperation((map_fd, key_ptr, value_ptr, flags))
+  ret = libc.syscall(__NR_bpf, BPF_MAP_UPDATE_ELEM, attr.CPointer(), len(attr))
+  return ret
+
+def LookupMap(map_fd, key):
+  _value = ctypes.c_uint32(0)
+  _key = ctypes.c_uint32(key)
+  attr = BpfAttrOperation((map_fd, ctypes.addressof(_key), ctypes.addressof(_value), 0))
+  ret = libc.syscall(__NR_bpf, BPF_MAP_LOOKUP_ELEM, attr.CPointer(), len(attr))
+  return ret, _value
+
+def GetNextKey(map_fd, key):
+  _key = ctypes.c_uint32(key)
+  _nextkey = ctypes.c_uint32(0)
+  attr = BpfAttrOperation((map_fd, ctypes.addressof(_key), ctypes.addressof(_nextkey), 0))
+  ret = libc.syscall(__NR_bpf, BPF_MAP_GET_NEXT_KEY, attr.CPointer(), len(attr))
+  return ret, _nextkey
+
+def DeleteMap(map_fd, key):
+  _key = ctypes.c_uint32(key);
+  attr = BpfAttrOperation((map_fd, ctypes.addressof(_key), 0, 0))
+  ret = libc.syscall(__NR_bpf, BPF_MAP_DELETE_ELEM, attr.CPointer(), len(attr))
+  return ret
+
+
+class BpfTest(net_test.NetworkTest):
+
+  @unittest.skipUnless(HAVE_EBPF_SUPPORT, "eBPF function not fully supported")
+  def testCreateMap(self):
+    key, value = 1, 1
+    map_fd = CreateMap(BPF_MAP_TYPE_HASH, 4, 4, 100)
+    self.assertGreater(map_fd, 0)
+    self.assertEquals(UpdateMap(map_fd, key, value), 0)
+    ret, result = LookupMap(map_fd, key)
+    self.assertEquals(ret, 0)
+    self.assertEquals(result.value, 1)
+    self.assertEquals(DeleteMap(map_fd, key), 0)
+    ret, result = LookupMap(map_fd, key)
+    self.assertEquals(ret, -1)
+    self.assertEquals(result.value, 0)
+
+  @unittest.skipUnless(HAVE_EBPF_SUPPORT, "eBPF function not fully supported")
+  def testIterateMap(self):
+    map_fd = CreateMap(BPF_MAP_TYPE_HASH, 4, 4, 100)
+    self.assertGreater(map_fd, 0)
+    value = 1024
+    for key in range(1, 100) :
+      self.assertEquals(UpdateMap(map_fd, key, value), 0)
+    for key in range(1, 100) :
+      ret, result = LookupMap(map_fd, key);
+      self.assertEquals(ret, 0)
+      self.assertEquals(result.value, value)
+    ret, result = LookupMap(map_fd, 101)
+    self.assertEquals(ret, -1)
+    self.assertEquals(result.value, 0)
+    key = 0
+    count = 0
+    ret, result = GetNextKey(map_fd, key)
+    while ret > -1 :
+      key = result.value
+      self.assertGreater(key, 0)
+      ret, retValue = LookupMap(map_fd, key)
+      self.assertEquals(ret, 0)
+      self.assertEquals(retValue.value, value)
+      count = count + 1
+      ret, result = GetNextKey(map_fd, key)
+    self.assertEquals(count, 99)
+
+if __name__ == "__main__":
+  unittest.main()
