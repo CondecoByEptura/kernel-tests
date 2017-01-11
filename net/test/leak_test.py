fc142dc0f8d4074865f41d7055ee88d593849f53
Lorenzo Colitti <lorenzo@google.com>
2017-01-11 17:53:25 +0900

    Check for negative values in SO_{RCV,SND}BUFFORCE.
    Test: passes on kernels that check for this.
    Bug: 33363517
    Change-Id: Ic903810279bc5b2802da660fb89ca1ca4668ede8

--- net/test/leak_test.py
+++ net/test/leak_test.py
@@ -16,6 +16,7 @@
 
 from errno import *  # pylint: disable=wildcard-import
 from socket import *  # pylint: disable=wildcard-import
+import struct
 import threading
 import time
 import unittest
@@ -45,6 +46,22 @@ class LeakTest(net_test.NetworkTest):
     data, addr = csocket.Recvfrom(s, 4096)
     self.assertEqual("", data)
     self.assertEqual(None, addr)
+
+  def testRcvBufForce(self):
+    SO_RCVBUFFORCE = 33
+
+    s = socket(AF_INET6, SOCK_DGRAM, 0)
+
+    s.setsockopt(SOL_SOCKET, SO_RCVBUF, 0)
+    minbuf = s.getsockopt(SOL_SOCKET, SO_RCVBUF)
+
+    val = 4097
+    s.setsockopt(SOL_SOCKET, SO_RCVBUFFORCE, val)
+    self.assertEquals(2 * val, s.getsockopt(SOL_SOCKET, SO_RCVBUF))
+
+    bogusval = 2 ** 31 - 5000
+    s.setsockopt(SOL_SOCKET, SO_RCVBUFFORCE, bogusval)
+    self.assertEquals(minbuf, s.getsockopt(SOL_SOCKET, SO_RCVBUF))
 
 
 if __name__ == "__main__":
