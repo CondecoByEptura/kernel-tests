4ea61b3e019a20b47e7df473dba5776be8f46ba2
Chenbo Feng <fengc@google.com>
2017-05-04 14:01:35 -0700

    Add tests for cgroup v2 bpf and new helper functions
    Add several bpf program test to verify the new get_socket_cookie and
    get_socket_uid helper function cherry-picked to 4.9 kernel. Also, added
    a new test class to test the cgroup v2 bpf functionality backported from
    upstream. Refactored the bpf assembly program into reusable code blocks.
    Added some helper function to avoid duplication of code. Added IPv6 support
    for all exisiting tests.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Bug: 30950746
    Test: All test added in this patch should pass in all 4.9 kernel
    
    Change-Id: I64ead3d1a04985e7499a1fe1cdee58ef580e9313

--- net/test/bpf_test.py
+++ net/test/bpf_test.py
@@ -18,18 +18,25 @@ import ctypes
 import errno
 import os
 import socket
+import struct
 import unittest
 
 from bpf import *  # pylint: disable=wildcard-import
 import csocket
 import net_test
+import sock_diag
 
 libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
 HAVE_EBPF_SUPPORT = net_test.LINUX_VERSION >= (4, 4, 0)
+HAVE_CGROUP_HELPER_SUPPORT = net_test.LINUX_VERSION >= (4, 9, 0)
+KEY_SIZE = 8;
+VALUE_SIZE = 4;
+TOTAL_ENTRIES = 20;
 
 @unittest.skipUnless(HAVE_EBPF_SUPPORT,
                      "eBPF function not fully supported")
 class BpfTest(net_test.NetworkTest):
+
 
   def setUp(self):
     self.map_fd = -1
@@ -40,27 +47,45 @@ class BpfTest(net_test.NetworkTest):
       os.close(self.prog_fd)
     if self.map_fd >= 0:
       os.close(self.map_fd)
+    if hasattr(self, 'sock'):
+      self.sock.close()
+
+  # A dummy loopback function to generate traffic through a socket.
+  def socketLoopBackWithFilter(self, packet_count, version):
+    family = {4: socket.AF_INET, 6: socket.AF_INET6}[version]
+    self.sock = socket.socket(family, socket.SOCK_DGRAM, 0)
+    BpfProgAttachSocket(self.sock.fileno(), self.prog_fd)
+    net_test.SetNonBlocking(self.sock)
+    addr = {4: "127.0.0.1", 6: "::1"}[version]
+    self.sock.bind((addr, 0))
+    addr = self.sock.getsockname()
+    sockaddr = csocket.Sockaddr(addr)
+    for i in xrange(packet_count):
+      self.sock.sendto("foo", addr)
+      data, retaddr = csocket.Recvfrom(self.sock, 4096, 0)
+      self.assertEqual("foo", data)
+      self.assertEqual(sockaddr, retaddr)
 
   def testCreateMap(self):
     key, value = 1, 1
-    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, 4, 4, 100)
+    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, KEY_SIZE, VALUE_SIZE, TOTAL_ENTRIES)
     UpdateMap(self.map_fd, key, value)
     self.assertEquals(LookupMap(self.map_fd, key).value, value)
     DeleteMap(self.map_fd, key)
     self.assertRaisesErrno(errno.ENOENT, LookupMap, self.map_fd, key)
 
   def testIterateMap(self):
-    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, 4, 4, 100)
+    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, KEY_SIZE, VALUE_SIZE, TOTAL_ENTRIES)
     value = 1024
-    for key in xrange(1, 100):
+    for key in xrange(1, TOTAL_ENTRIES):
       UpdateMap(self.map_fd, key, value)
-    for key in xrange(1, 100):
+    for key in xrange(1, TOTAL_ENTRIES):
       self.assertEquals(LookupMap(self.map_fd, key).value, value)
     self.assertRaisesErrno(errno.ENOENT, LookupMap, self.map_fd, 101)
     key = 0
     count = 0
     while 1:
-      if count == 99:
+      if count == TOTAL_ENTRIES - 1:
         self.assertRaisesErrno(errno.ENOENT, GetNextKey, self.map_fd, key)
         break
       else:
@@ -71,93 +96,162 @@ class BpfTest(net_test.NetworkTest):
         count += 1
 
   def testProgLoad(self):
-    bpf_prog = BpfMov64Reg(BPF_REG_6, BPF_REG_1)
-    bpf_prog += BpfLdxMem(BPF_W, BPF_REG_0, BPF_REG_6, 0)
-    bpf_prog += BpfExitInsn()
-    insn_buff = ctypes.create_string_buffer(bpf_prog)
-    # Load a program that does nothing except pass every packet it receives
-    # It should not block the packet transmission otherwise the test fails.
-    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER,
-                          ctypes.addressof(insn_buff),
-                          len(insn_buff), BpfInsn._length)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-    sock.settimeout(1)
-    BpfProgAttachSocket(sock.fileno(), self.prog_fd)
-    addr = "127.0.0.1"
-    sock.bind((addr, 0))
-    addr = sock.getsockname()
-    sockaddr = csocket.Sockaddr(addr)
-    sock.sendto("foo", addr)
-    data, addr = csocket.Recvfrom(sock, 4096, 0)
-    self.assertEqual("foo", data)
-    self.assertEqual(sockaddr, addr)
+    # Move skb to BPF_REG_6 for further usage
+    instructions = [
+        BpfMov64Reg(BPF_REG_6, BPF_REG_1)
+    ]
+    instructions += insSkFilterAccept
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER, instructions)
+    self.socketLoopBackWithFilter(1, 4)
+    self.socketLoopBackWithFilter(1, 6)
 
   def testPacketBlock(self):
-    bpf_prog = BpfMov64Reg(BPF_REG_6, BPF_REG_1)
-    bpf_prog += BpfMov64Imm(BPF_REG_0, 0)
-    bpf_prog += BpfExitInsn()
-    insn_buff = ctypes.create_string_buffer(bpf_prog)
-    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER,
-                          ctypes.addressof(insn_buff),
-                          len(insn_buff), BpfInsn._length)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-    sock.settimeout(1)
-    BpfProgAttachSocket(sock.fileno(), self.prog_fd)
-    addr = "127.0.0.1"
-    sock.bind((addr, 0))
-    addr = sock.getsockname()
-    sock.sendto("foo", addr)
-    self.assertRaisesErrno(errno.EAGAIN, csocket.Recvfrom, sock, 4096, 0)
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER, insBpfExitBlock)
+    self.assertRaisesErrno(errno.EAGAIN, self.socketLoopBackWithFilter, 1, 4)
+    self.assertRaisesErrno(errno.EAGAIN, self.socketLoopBackWithFilter, 1, 6)
 
   def testPacketCount(self):
-    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, 4, 4, 10)
+    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, KEY_SIZE, VALUE_SIZE, TOTAL_ENTRIES)
     key = 0xf0f0
-    bpf_prog = BpfMov64Reg(BPF_REG_6, BPF_REG_1)
-    bpf_prog += BpfLoadMapFd(self.map_fd, BPF_REG_1)
-    bpf_prog += BpfMov64Imm(BPF_REG_7, key)
-    bpf_prog += BpfStxMem(BPF_W, BPF_REG_10, BPF_REG_7, -4)
-    bpf_prog += BpfMov64Reg(BPF_REG_8, BPF_REG_10)
-    bpf_prog += BpfAlu64Imm(BPF_ADD, BPF_REG_8, -4)
-    bpf_prog += BpfMov64Reg(BPF_REG_2, BPF_REG_8)
-    bpf_prog += BpfFuncLookupMap()
-    bpf_prog += BpfJumpImm(BPF_AND, BPF_REG_0, 0, 10)
-    bpf_prog += BpfLoadMapFd(self.map_fd, BPF_REG_1)
-    bpf_prog += BpfMov64Reg(BPF_REG_2, BPF_REG_8)
-    bpf_prog += BpfStMem(BPF_W, BPF_REG_10, -8, 1)
-    bpf_prog += BpfMov64Reg(BPF_REG_3, BPF_REG_10)
-    bpf_prog += BpfAlu64Imm(BPF_ADD, BPF_REG_3, -8)
-    bpf_prog += BpfMov64Imm(BPF_REG_4, 0)
-    bpf_prog += BpfFuncUpdateMap()
-    bpf_prog += BpfLdxMem(BPF_W, BPF_REG_0, BPF_REG_6, 0)
-    bpf_prog += BpfExitInsn()
-    bpf_prog += BpfMov64Reg(BPF_REG_2, BPF_REG_0)
-    bpf_prog += BpfMov64Imm(BPF_REG_1, 1)
-    bpf_prog += BpfRawInsn(BPF_STX | BPF_XADD | BPF_W, BPF_REG_2, BPF_REG_1,
-                           0, 0)
-    bpf_prog += BpfLdxMem(BPF_W, BPF_REG_0, BPF_REG_6, 0)
-    bpf_prog += BpfExitInsn()
-    insn_buff = ctypes.create_string_buffer(bpf_prog)
-    # this program loaded is used to counting the packet transmitted through
-    # a target socket. It will store the packet count into the eBPF map and we
-    # will verify if the counting result is correct.
-    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER,
-                          ctypes.addressof(insn_buff),
-                          len(insn_buff), BpfInsn._length)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-    sock.settimeout(1)
-    BpfProgAttachSocket(sock.fileno(), self.prog_fd)
-    addr = "127.0.0.1"
+    # Set up instruction block with key loaded at BPF_REG_0.
+    instructions = [
+        BpfMov64Reg(BPF_REG_6, BPF_REG_1),
+        BpfMov64Imm(BPF_REG_0, key)
+    ]
+    # Concatenate the generic packet count bpf program to it.
+    instructions += (BpfFuncCountPacketInit(self.map_fd) + insSkFilterAccept
+                     + insPackCountUpdate + insSkFilterAccept)
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER, instructions)
+    packet_count = 10
+    self.socketLoopBackWithFilter(packet_count, 4)
+    self.socketLoopBackWithFilter(packet_count, 6)
+    self.assertEquals(LookupMap(self.map_fd, key).value, packet_count*2)
+
+  @unittest.skipUnless(HAVE_CGROUP_HELPER_SUPPORT,
+                       "BPF helper function is not fully supported")
+  def testGetSocketCookie(self):
+    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, KEY_SIZE, VALUE_SIZE, TOTAL_ENTRIES)
+    instructions = [
+        BpfMov64Reg(BPF_REG_6, BPF_REG_1),
+        BpfFuncGetSocketCookie()
+    ]
+    instructions += (BpfFuncCountPacketInit(self.map_fd) + insSkFilterAccept
+                     + insPackCountUpdate + insSkFilterAccept)
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER, instructions)
+    packet_count = 10;
+    # Test the program with IPv4.
+    self.socketLoopBackWithFilter(packet_count, 4)
+    self.sock_diag = sock_diag.SockDiag()
+    real_cookie = self.sock_diag.FindSockDiagFromFd(self.sock).id.cookie
+    cookie = struct.unpack('=Q', real_cookie)[0]
+    self.assertEquals(LookupMap(self.map_fd, cookie).value, packet_count)
+    self.sock.close()
+    # Test the program with IPv6, it uses a different socket to do the loopback
+    # so the socket cookie will be different.
+    self.socketLoopBackWithFilter(packet_count, 6)
+    self.sock_diag = sock_diag.SockDiag()
+    real_cookie = self.sock_diag.FindSockDiagFromFd(self.sock).id.cookie
+    cookie = struct.unpack('=Q', real_cookie)[0]
+    self.assertEquals(LookupMap(self.map_fd, cookie).value, packet_count)
+
+  @unittest.skipUnless(HAVE_CGROUP_HELPER_SUPPORT,
+                       "BPF helper function is not fully supported")
+  def testGetSocketUid(self):
+    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, KEY_SIZE, VALUE_SIZE, TOTAL_ENTRIES)
+    # Set up the instruction with uid at BPF_REG_0.
+    instructions = [
+        BpfMov64Reg(BPF_REG_6, BPF_REG_1),
+        BpfFuncGetSocketUid()
+    ]
+    # Concatenate the generic packet count bpf program to it.
+    instructions += (BpfFuncCountPacketInit(self.map_fd) + insSkFilterAccept
+                     + insPackCountUpdate + insSkFilterAccept)
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_SOCKET_FILTER, instructions)
+    packet_count = 10;
+    self.socketLoopBackWithFilter(packet_count, 4)
+    uid = os.getuid()
+    self.assertEquals(LookupMap(self.map_fd, uid).value, packet_count)
+    self.socketLoopBackWithFilter(packet_count, 6)
+    self.assertEquals(LookupMap(self.map_fd, uid).value, 2*packet_count)
+
+@unittest.skipUnless(HAVE_CGROUP_HELPER_SUPPORT,
+                     "Cgroup BPF is not fully supported")
+class BpfCgroupTest(net_test.NetworkTest):
+
+  def setUp(self):
+    if not os.path.isdir("/tmp"):
+      os.mkdir('/tmp')
+    os.system('mount -t cgroup2 cg_bpf /tmp')
+    self.prog_fd = -1
+    self.map_fd = -1
+    self.cg_fd = os.open('/tmp', os.O_DIRECTORY | os.O_RDONLY)
+
+  def tearDown(self):
+    if self.prog_fd >= 0:
+      os.close(self.prog_fd)
+    if self.map_fd >= 0:
+      os.close(self.map_fd)
+    BpfProgDetach(self.cg_fd, BPF_CGROUP_INET_EGRESS)
+    BpfProgDetach(self.cg_fd, BPF_CGROUP_INET_INGRESS)
+    os.close(self.cg_fd)
+    os.system('umount cg_bpf')
+
+  # a similiar loopback helper without sk filter attached.
+  def socketLoopBack(self, packet_count, version):
+    family = {4: socket.AF_INET, 6: socket.AF_INET6}[version]
+    sock = socket.socket(family, socket.SOCK_DGRAM, 0)
+    net_test.SetNonBlocking(sock)
+    addr = {4: "127.0.0.1", 6: "::1"}[version]
     sock.bind((addr, 0))
     addr = sock.getsockname()
     sockaddr = csocket.Sockaddr(addr)
-    packet_count = 100
     for i in xrange(packet_count):
       sock.sendto("foo", addr)
       data, retaddr = csocket.Recvfrom(sock, 4096, 0)
       self.assertEqual("foo", data)
       self.assertEqual(sockaddr, retaddr)
-    self.assertEquals(LookupMap(self.map_fd, key).value, packet_count)
+    sock.close()
 
+  def testCgBpfAttach(self):
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_CGROUP_SKB, insBpfExitBlock)
+    BpfProgAttach(self.prog_fd, self.cg_fd, BPF_CGROUP_INET_INGRESS)
+    BpfProgDetach(self.cg_fd, BPF_CGROUP_INET_INGRESS)
+
+  def testCgroupIngress(self):
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_CGROUP_SKB, insBpfExitBlock)
+    BpfProgAttach(self.prog_fd, self.cg_fd, BPF_CGROUP_INET_INGRESS)
+    self.assertRaisesErrno(errno.EAGAIN, self.socketLoopBack, 1, 4)
+    self.assertRaisesErrno(errno.EAGAIN, self.socketLoopBack, 1, 6)
+    BpfProgDetach(self.cg_fd, BPF_CGROUP_INET_INGRESS)
+    self.socketLoopBack(1, 4)
+    self.socketLoopBack(1, 6)
+
+  def testCgroupEgress(self):
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_CGROUP_SKB, insBpfExitBlock)
+    BpfProgAttach(self.prog_fd, self.cg_fd, BPF_CGROUP_INET_EGRESS)
+    self.assertRaisesErrno(errno.EPERM, self.socketLoopBack, 1, 4)
+    self.assertRaisesErrno(errno.EPERM, self.socketLoopBack, 1, 6)
+    BpfProgDetach(self.cg_fd, BPF_CGROUP_INET_EGRESS)
+    self.socketLoopBack( 1, 4)
+    self.socketLoopBack( 1, 6)
+
+  def testCgroupBpfUid(self):
+    self.map_fd = CreateMap(BPF_MAP_TYPE_HASH, KEY_SIZE, VALUE_SIZE, TOTAL_ENTRIES)
+    # The same eBPF program used in socket uid test. 
+    instructions = [
+        BpfMov64Reg(BPF_REG_6, BPF_REG_1),
+        BpfFuncGetSocketUid(),
+    ]
+    instructions += (BpfFuncCountPacketInit(self.map_fd) + insCgroupAccept
+                     + insPackCountUpdate + insCgroupAccept)
+    self.prog_fd = BpfProgLoad(BPF_PROG_TYPE_CGROUP_SKB, instructions)
+    BpfProgAttach(self.prog_fd, self.cg_fd, BPF_CGROUP_INET_INGRESS)
+    uid = os.getuid()
+    packet_count = 20
+    self.socketLoopBack(packet_count, 4)
+    self.socketLoopBack(packet_count, 6)
+    self.assertEquals(LookupMap(self.map_fd, uid).value, packet_count*2)
+    BpfProgDetach(self.cg_fd, BPF_CGROUP_INET_INGRESS)
 
 if __name__ == "__main__":
   unittest.main()
