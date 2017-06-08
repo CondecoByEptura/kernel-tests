de3c7fd3f745bd53ff0106fac0e60e3ea9aa751f
Nathan Harold <nharold@google.com>
2017-06-07 17:53:25 -0700

    Add Tests for XFRM Tunnel and VTI Interface
    Tests Added -
    -Create an xfrm tunnel and send encrypted packet
    -Create and teardown a vti
    -Create a vti+xfrm tunnel and send a packet through it
    
    Bug: 62633844
    Test: this
    Change-Id: Ifb98cfed73a94655a2c244941ea3b041f0c2a78f

--- net/test/xfrm_base.py
+++ net/test/xfrm_base.py
@@ -15,15 +15,59 @@
 # limitations under the License.
 
 import multinetwork_base
+import random
+import socket
 import struct
 import xfrm
 
+_ENCRYPTION_KEY_256 = ("308146eb3bd84b044573d60f5a5fd159"
+                       "57c7d4fe567a2120f35bae0f9869ec22".decode("hex"))
+_AUTHENTICATION_KEY_128 = "af442892cdcd0ef650e9c299f9a8436a".decode("hex")
+
+_ALGO_CBC_AES_256 = xfrm.XfrmAlgo(("cbc(aes)", 256))
+_ALGO_HMAC_SHA1 = xfrm.XfrmAlgoAuth(("hmac(sha1)", 128, 96))
+
+# Match all bits of the mark
+_MARK_MASK_ALL = 0xffffffff
+
+_ALL_ALGORITHMS = 0xffffffff
+
+
 class XfrmBaseTest(multinetwork_base.MultiNetworkBaseTest):
-  _ENCRYPTION_KEY_256 = ("308146eb3bd84b044573d60f5a5fd159"
-                     "57c7d4fe567a2120f35bae0f9869ec22".decode("hex"))
-  _AUTH_KEY_128 = "af442892cdcd0ef650e9c299f9a8436a".decode("hex")
 
-  _ALL_ALGORITHMS = 0xffffffff
-  _ALGO_CBC_AES_256 = xfrm.XfrmAlgo(("cbc(aes)", 256))
-  _ALGO_HMAC_SHA1 = xfrm.XfrmAlgoAuth(("hmac(sha1)", 128, 96))
+  def setUp(self):
+    super(XfrmBaseTest, self).setUp()
 
+  def tearDown(self):
+    super(XfrmBaseTest, self).tearDown()
+
+  def _RandomNetid(self):
+    return random.choice(self.NETIDS)
+
+  @staticmethod
+  def _GetWildcardAddress(version):
+    return {4: "0.0.0.0", 6: "::"}[version]
+
+  @staticmethod
+  def _GetAddressFamily(version):
+    return {4: socket.AF_INET, 6: socket.AF_INET6}[version]
+
+  def _ExpectEspPacketOn(self, netid, spi, seq, length, src_addr, dst_addr):
+    packets = self.ReadAllPacketsOn(netid)
+    self.assertEquals(1, len(packets))
+    packet = packets[0]
+    payload = str(packet.payload)[8:]
+    if length is not None:
+      self.assertEquals(length,
+                        len(packet.payload), "expected length=%d, received=%d" %
+                        (length, len(packet.payload)))
+    if dst_addr is not None:
+      self.assertEquals(dst_addr, packet.dst,
+                        "expected dst addr=%s, received=%s" % (dst_addr,
+                                                               packet.dst))
+    if src_addr is not None:
+      self.assertEquals(src_addr, packet.src,
+                        "expected src addr=%s, received=%s" % (src_addr,
+                                                               packet.src))
+    esp_hdr = xfrm.EspHdr(str(packet.payload))
+    self.assertEquals(xfrm.EspHdr((spi, seq)), esp_hdr)
