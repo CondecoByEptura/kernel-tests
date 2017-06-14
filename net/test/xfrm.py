e5751cf5868715ee9a93348b561b90829965af6b
Nathan Harold <nharold@google.com>
2017-06-14 13:13:27 -0700

    Add Tests for XFRM Tunnel and VTI Interface
    Tests Added -
    -Add methods for creating and deleting VTIs
    -Create an xfrm tunnel and send encrypted packet
    -Create and teardown a vti
    -Create a vti+xfrm tunnel and send a packet through it
    
    Bug: 62633844
    Test: this
    Change-Id: Ifb98cfed73a94655a2c244941ea3b041f0c2a78f

--- net/test/xfrm.py
+++ net/test/xfrm.py
@@ -29,9 +29,6 @@ import netlink
 # TODO: move this somewhere more appropriate when possible
 EspHdr = cstruct.Struct("EspHdr", "!II", "spi seqnum")
 
-# Base netlink constants. See include/uapi/linux/netlink.h.
-NETLINK_XFRM = 6
-
 # Netlink constants. See include/uapi/linux/xfrm.h.
 # Message types.
 XFRM_MSG_NEWSA = 16
@@ -218,11 +215,10 @@ def PaddedAddress(addr):
 class Xfrm(netlink.NetlinkSocket):
   """Netlink interface to xfrm."""
 
-  FAMILY = NETLINK_XFRM
   DEBUG = False
 
   def __init__(self):
-    super(Xfrm, self).__init__()
+    super(Xfrm, self).__init__(netlink.NETLINK_XFRM)
 
   def _GetConstantName(self, value, prefix):
     return super(Xfrm, self)._GetConstantName(__name__, value, prefix)
