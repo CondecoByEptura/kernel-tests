415f44ff49e4ee84eef575cbf55563dfac8ca196
Lorenzo Colitti <lorenzo@google.com>
2017-09-08 10:55:50 +0900

    DO NOT SUBMIT: rewrite InvalidateDstCache.
    Instead of pointing a route at the specified destination,
    create a low-priority throw route.
    
    Test: all_tests.sh passes on bullhead.
    Change-Id: I3da59776a4388ccec0131c4e223b76fec9c4727f

--- net/test/multinetwork_base.py
+++ net/test/multinetwork_base.py
@@ -504,10 +504,11 @@ class MultiNetworkBaseTest(net_test.NetworkTest):
     return packets
 
   def InvalidateDstCache(self, version, remoteaddr, netid):
-    """Invalidates destination cache entries of sockets to remoteaddr.
+    """Invalidates destination cache entries of sockets on the specified table.
 
-    Creates and then deletes a route pointing to remoteaddr, which invalidates
-    the destination cache entries of any sockets connected to remoteaddr.
+    Creates and then deletes a low-priority unreachable route in the given
+    netid, which invalidates the destination cache entries of any sockets that
+    refer to that table.
     The fact that this method actually invalidates destination cache entries is
     tested by OutgoingTest#testIPv[46]Remarking, which checks that the kernel
     does not re-route sockets when they are remarked, but does re-route them if
@@ -515,16 +516,20 @@ class MultiNetworkBaseTest(net_test.NetworkTest):
 
     Args:
       version: The IP version, 4 or 6.
-      remoteaddr: The IP address to temporarily reroute.
+      remoteaddr: Unused.
       netid: The netid to add/remove the route to.
     """
     iface = self.GetInterfaceName(netid)
     ifindex = self.ifindices[netid]
     table = self._TableForNetid(netid)
-    nexthop = self._RouterAddress(netid, version)
-    plen = {4: 32, 6: 128}[version]
-    self.iproute.AddRoute(version, table, remoteaddr, plen, nexthop, ifindex)
-    self.iproute.DelRoute(version, table, remoteaddr, plen, nexthop, ifindex)
+    self.iproute._Route(version, iproute.RTPROT_STATIC, iproute.RTM_NEWROUTE,
+                        table, "default", 0, nexthop=None, dev=None, mark=None,
+                        uid=None, route_type=iproute.RTN_THROW,
+                        priority=100000)
+    self.iproute._Route(version, iproute.RTPROT_STATIC, iproute.RTM_DELROUTE,
+                        table, "default", 0, nexthop=None, dev=None, mark=None,
+                        uid=None, route_type=iproute.RTN_THROW,
+                        priority=100000)
 
   def ClearTunQueues(self):
     # Keep reading packets on all netids until we get no packets on any of them.
