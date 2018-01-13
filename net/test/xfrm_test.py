10cf9e65117c4a5f7a8b1f9642acdfa52cdd10df
Lorenzo Colitti <lorenzo@google.com>
2018-01-13 17:11:09 +0900

    Add the mark to DeleteSaInfo.
    This addresses a TODO and is needed for upcoming changes that
    rely on deleting tunnels.
    
    Test: xfrm tests pass on android-4.9
    Change-Id: I82d47fff2212f416b188275afc085c56298fb99c

--- net/test/xfrm_test.py
+++ net/test/xfrm_test.py
@@ -87,7 +87,7 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
     try:
       self.assertMultiLineEqual(expected, actual)
     finally:
-      self.xfrm.DeleteSaInfo(TEST_ADDR1, TEST_SPI, IPPROTO_ESP)
+      self.xfrm.DeleteSaInfo(TEST_ADDR1, TEST_SPI, IPPROTO_ESP, None)
 
   def testFlush(self):
     self.assertEquals(0, len(self.xfrm.DumpSaInfo()))
@@ -167,7 +167,7 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
 
     # Deleting the SA causes the first socket to return errors again.
     self.xfrm.DeleteSaInfo(self.GetRemoteAddress(xfrm_version), TEST_SPI,
-                           IPPROTO_ESP)
+                           IPPROTO_ESP, None)
     self.assertRaisesErrno(
         EAGAIN,
         s.sendto, net_test.UDP_PAYLOAD, (remotesockaddr, 53))
@@ -378,8 +378,8 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
                                start_spi, rekey_spi, True, 2)
 
     # Delete old SAs
-    self.xfrm.DeleteSaInfo(remoteaddr, start_spi, IPPROTO_ESP)
-    self.xfrm.DeleteSaInfo(myaddr, start_spi, IPPROTO_ESP)
+    self.xfrm.DeleteSaInfo(remoteaddr, start_spi, IPPROTO_ESP, None)
+    self.xfrm.DeleteSaInfo(myaddr, start_spi, IPPROTO_ESP, None)
 
     # Check that UDP encap socket works with updated socket policy and new SAs
     self._VerifyUdpEncapSocket(netid, remoteaddr, myaddr, encap_port, s,
