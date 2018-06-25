c90d4c242a61acd72329ba3818ff8df0a4a38802
Hsin-Yi Chen <hsinyichen@google.com>
2018-06-25 18:42:21 +0800

    Fix net tests for 32-bit kernel
    - Add bpf syscall number for arm7l.
    - Ignore non-numeric suffix of kernel version.
    
    Bug: 110440652
    Test: run vts -m VtsKernelNetTest
    Change-Id: If217c961fdd45b013a7d83967bcab8c746804b97

--- net/test/csocket.py
+++ net/test/csocket.py
@@ -80,7 +80,7 @@ libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
 def LinuxVersion():
   # Example: "3.4.67-00753-gb7a556f".
   # Get the part before the dash.
-  version = os.uname()[2].split("-")[0]
+  version = re.search("^[0-9.]*", os.uname()[2]).group()
   # Convert it into a tuple such as (3, 4, 67). That allows comparing versions
   # using < and >, since tuples are compared lexicographically.
   version = tuple(int(i) for i in version.split("."))
