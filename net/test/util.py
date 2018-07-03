658589e393154d31b5c8d84c4f6cc1b94b5dc7af
Benedict Wong <benedictwong@google.com>
2018-07-03 15:27:26 -0700

    Refactor parameterization logic in net tests
    This change refactors the logic to create parameterized tests in the
    kernel unit tests. Logic for name generation is left to classes, while
    common code for test injection is moved to the utility class
    
    Bug: 66467511
    Test: Ran net tests
    Change-Id: I7eba57c616145246637beefac3aca16f9e2e899e

--- net/test/util.py
+++ net/test/util.py
@@ -14,3 +14,35 @@
 
 def GetPadLength(block_size, length):
   return (block_size - (length % block_size)) % block_size
+
+def InjectParameterizedTest(cls, param_list, name_generator):
+  """Injects parameterized tests into the provided class
+
+  This method searches for all tests that start with the name "ParamTest",
+  and injects a test method for each pair of parameters in param_list. Names
+  are generated via the use of the name_generator.
+
+  Args:
+    cls: the class for which to inject all paramterized tests
+    param_list: a 2d array of parameters for each test run
+    name_generator: the function to generate parameterized test names
+  """
+  param_test_names = [name for name in dir(cls) if name.startswith("ParamTest")]
+
+  # Parameterize each test method starting with "ParamTest"
+  for test_name in param_test_names:
+    func = getattr(cls, test_name)
+
+    for params in param_list:
+      # Generate the actual test method.
+      def TestClosure(self):
+        func(self, *params)
+
+      # Give the test method a readable, debuggable name.
+      param_string = name_generator(*params)
+      new_name = "%s_%s" % (func.__name__.replace("ParamTest", "test"),
+                            param_string)
+      new_name = new_name.replace("(", "-").replace(")", "")  # remove parens
+
+      # Inject the test method
+      setattr(cls, new_name, TestClosure)
