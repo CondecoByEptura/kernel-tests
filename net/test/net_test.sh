0674bd925dac631ef6b417f1980cae0b9347cbc1
Maciej Żenczykowski <maze@google.com>
2018-10-14 20:57:52 -0700

    run_net_test: UML - fix insufficient entropy problems
    This resolves IPv6 ping problems on recent (for example 4.17) kernels.
    
    Change-Id: I5310bad2f3148d1195367aade10b9761995134a4

--- net/test/net_test.sh
+++ net/test/net_test.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+add_entropy() {
+  local -r bits_of_entropy_per_character="$1"
+  local -r random_characters="$2"
+
+  # kernel/include/uapi/linux/random.h: RNDADDENTROPY = _IOW('R', 0x03, int [2]) =(R is 0x52)= 0x40085203 = 1074287107
+  python <<-EOF
+	import fcntl, os, struct
+	RNDADDENTROPY = 1074287107
+	fd = os.open('/dev/random', os.O_WRONLY)
+	rnd = '${random_characters}'
+	entropy = len(rnd) * ${bits_of_entropy_per_character}
+	fcntl.ioctl(fd, RNDADDENTROPY, struct.pack('ii%is' % len(rnd), entropy, len(rnd), rnd))
+	os.close(fd)
+EOF
+}
+
+if [[ -n "${random_initializer}" ]]; then
+  echo "entropy available: $(< /proc/sys/kernel/random/entropy_avail)" 1>&2
+  echo "adding entropy from hex string [${random_initializer}]" 1>&2
+  # We assume the string is a hex encoded string, hence only 4 bits of entropy
+  # per character. Experience shows we need at least 128 bits of entropy for
+  # crng init to complete, hence we need at least 32 hex chars from kcmdline,
+  # we'll get 64 just to be safe.
+  add_entropy 4 "${random_initializer}"
+  # Give the system a moment to catch up.
+  sleep 0.2
+  echo "entropy available: $(< /proc/sys/kernel/random/entropy_avail)" 1>&2
+fi
 
 # In case IPv6 is compiled as a module.
 [ -f /proc/net/if_inet6 ] || insmod $DIR/kernel/net-next/net/ipv6/ipv6.ko
