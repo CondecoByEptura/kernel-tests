00a0fc2b70de758f61993b02243ce44ecdb0ebfb
Maciej Żenczykowski <maze@google.com>
2018-11-07 02:16:37 -0800

    net_test - fix job control in console's bash terminal
    This makes Ctrl+C and Ctrl+Z work.
    
    (while we're at it add some more debugging print out to ease
     debugging and better explain how the transition works)
    
    Change-Id: I5d6d82a53161f7ff1bc6f1b5329ba864d0759a48

--- net/test/net_test.sh
+++ net/test/net_test.sh
@@ -1,4 +1,139 @@
 #!/bin/bash
+echo "uname -a == [$(uname -a)]"
+echo
+
+echo "kernel command line [$(< /proc/cmdline)]"
+echo
+
+
+echo 'Current working directory:'
+echo " - according to PWD:      [${PWD}]"
+echo " - according to builtin:  [$(pwd)]"
+echo " - according to /bin/pwd: [$(/bin/pwd)]"
+echo
+
+echo "PATH[${PATH}]"
+echo
+
+echo 'shell environment:'
+env
+echo
+
+echo -n "net_test.sh (pid $$, parent ${PPID}, tty $(tty)) running [$0] with args:"
+for arg in "$@"; do
+  echo -n " [${arg}]"
+done
+echo
+echo
+
+if [[ "$(tty)" == '/dev/console' ]]; then
+  ARCH="$(uname -m)"
+  # Underscore is illegal in hostname, replace with hyphen
+  ARCH="${ARCH//_/-}"
+
+  # setsid + /dev/tty{,AMA,S}0 allows bash's job control to work, ie. Ctrl+C/Z
+  if [[ -c '/dev/tty0' ]]; then
+    # exists in UML, does not exist on graphics/vga/curses-less QEMU
+    CON='/dev/tty0'
+    hostname "uml-${ARCH}"
+  elif [[ -c '/dev/ttyAMA0' ]]; then
+    # Qemu for arm (note: /dev/ttyS0 also exists for exitcode)
+    CON='/dev/ttyAMA0'
+    hostname "qemu-${ARCH}"
+  elif [[ -c '/dev/ttyS0' ]]; then
+    # Qemu for x86 (note: /dev/ttyS1 also exists for exitcode)
+    CON='/dev/ttyS0'
+    hostname "qemu-${ARCH}"
+  else
+    # Can't figure it out, job control won't work, tough luck
+    echo 'Unable to figure out proper console - job control will not work.' 1>&2
+    CON=''
+    hostname "local-${ARCH}"
+  fi
+
+  unset ARCH
+
+  echo "Currently tty[/dev/console], but it should be [${CON}]..."
+
+  if [[ -n "${CON}" ]]; then
+    # Redirect std{in,out,err} to the console equivalent tty
+    # which actually supports all standard tty ioctls
+    exec <"${CON}" >&"${CON}"
+
+    # Re-executing if we were called with -c is too hard, hence this extra
+    # check, but this should not happen due to how image is formed...
+    if [[ -z "${BASH_EXECUTION_STRING}" ]]; then
+      # Bash wants to be session leader, hence need for setsid
+      echo "Re-executing..."
+      exec /usr/bin/setsid "$0" "$@"
+      # If the above exec fails, we just fall through...
+      # (this implies failure to *find* setsid, not error return from bash,
+      #  in practice due to image construction this cannot happen)
+    fi
+  fi
+
+  # In case we fall through, clean up
+  unset CON
+fi
+
+init_columns_and_lines() {
+  # We want to initialize LINES and COLUMNS even though we're not interactive
+  shopt -s checkwinsize
+
+  # Actually force the initialization... (yes, the 'cat' is needed... don't ask)
+  set | cat > /dev/null
+
+  # And turn it back off again...
+  shopt -u checkwinsize
+}
+
+echo 'TTY settings (concise):'
+stty
+echo
+
+echo 'TTY settings (verbose):'
+stty -a
+echo
+
+init_columns_and_lines
+echo "COLUMNS[${COLUMNS}] LINES[${LINES}]"
+echo
+
+echo 'Restoring TTY sanity...'
+stty sane
+stty 115200
+
+get_kcmdline() {
+  sed -rn "s@^(.* )?$1=([^ ]*)( .*)?\$@\2@p" < /proc/cmdline
+}
+
+COLS="$(get_kcmdline console_columns)"
+[[ -z "${COLS}" ]] || stty columns "${COLS}"
+unset COLS
+
+ROWS="$(get_kcmdline console_rows)"
+[[ -z "${ROWS}" ]] || stty rows "${ROWS}"
+unset ROWS
+
+echo 'TTY settings (concise):'
+stty
+echo
+
+echo 'TTY settings (verbose):'
+stty -a
+echo
+
+init_columns_and_lines
+echo "COLUMNS[${COLUMNS}] LINES[${LINES}]"
+echo
+
+# By the time we get here we should have a sane console:
+#  - 115200 baud rate
+#  - appropriate (and known) width and height (note: this assumes
+#    that the terminal doesn't get further resized)
+#  - it is no longer /dev/console, so job control should function
+#    (this means working ctrl+c [abort] and ctrl+z [suspend])
+
 if [[ -n "${entropy}" ]]; then
   echo "adding entropy from hex string [${entropy}]" 1>&2
 
