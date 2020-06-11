f9931a74be60c9e9f05a2db9de1ad6a0ead8dad9
nelsonli <nelsonli@google.com>
2020-06-11 15:06:38 +0800

    net-test: be more compatible with python3
    Conversions are primarily:
      print ... -> print(...)
      print ..., -> print(..., end=' ')
      xrange(...) -> range(...)
      assertEquals(...) -> AssertEqual(...)
      except t, e -> except t as e
      "\x00" -> "\x00".encode()
      iteritems() -> items()
    
    Plus:
      sprinkle in some list()'s around a few things
      and a few other minor things
    
    Test: tested on ACK5.4 with 32 and 64-bit x86 uml
      which proves this continues to work with the uml image's embedded python2
    Bug: 158494111
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I89dffc575293c1768eacdc6f1bce2bee4095c590

--- net/test/namespace.py
+++ net/test/namespace.py
@@ -112,10 +112,10 @@ def UnShare(flags):
 
 
 def DumpMounts(hdr):
-  print
-  print hdr
-  print open('/proc/mounts', 'r').read(),
-  print '---'
+  print()
+  print(hdr)
+  print(open('/proc/mounts', 'r').read(), end=' ')
+  print('---')
 
 
 # Requires at least kernel configuration options:
@@ -125,12 +125,12 @@ def DumpMounts(hdr):
 def IfPossibleEnterNewNetworkNamespace():
   """Instantiate and transition into a fresh new network namespace if possible."""
 
-  print 'Creating clean namespace...',
+  print('Creating clean namespace...', end=' ')
 
   try:
     UnShare(CLONE_NEWNS | CLONE_NEWUTS | CLONE_NEWNET)
   except OSError as err:
-    print 'failed: %s (likely: no privs or lack of kernel support).' % err
+    print('failed: %s (likely: no privs or lack of kernel support).' % err)
     return False
 
   try:
@@ -143,11 +143,11 @@ def IfPossibleEnterNewNetworkNamespace():
     SetFileContents('/proc/sys/net/ipv4/ping_group_range', '0 2147483647')
     net_test.SetInterfaceUp('lo')
   except:
-    print 'failed.'
+    print('failed.')
     # We've already transitioned into the new netns -- it's too late to recover.
     raise
 
-  print 'succeeded.'
+  print('succeeded.')
   return True
 
 
