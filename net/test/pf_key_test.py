5b87b08bc5c4334452b934fb4f6f672c3d467db7
Krzysztof Kosiński <krzysio@google.com>
2022-09-17 11:40:52 +0000

    Port kernel networking tests to Python 3.
    Bug: 203436762
    Bug: 243057700
    Test: atest vts_kernel_net_tests
    Change-Id: I69c03747656790e39ba8c128652821e2b51c7b60

--- net/test/pf_key_test.py
+++ net/test/pf_key_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # Copyright 2017 The Android Open Source Project
 #
@@ -22,9 +22,9 @@ import csocket
 import pf_key
 import xfrm
 
-ENCRYPTION_KEY = ("308146eb3bd84b044573d60f5a5fd159"
-                  "57c7d4fe567a2120f35bae0f9869ec22".decode("hex"))
-AUTH_KEY = "af442892cdcd0ef650e9c299f9a8436a".decode("hex")
+ENCRYPTION_KEY = bytes.fromhex("308146eb3bd84b044573d60f5a5fd159"
+                               "57c7d4fe567a2120f35bae0f9869ec22")
+AUTH_KEY = bytes.fromhex("af442892cdcd0ef650e9c299f9a8436a")
 
 
 class PfKeyTest(unittest.TestCase):
