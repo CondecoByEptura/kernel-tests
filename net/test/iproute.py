bb29a9340f7a7cf3af1369dfc4832351286b5dba
Lorenzo Colitti <lorenzo@google.com>
2022-11-18 18:35:04 +0900

    Support overlapping enum prefixes in _GetConstantName.
    Currently, _GetConstantName will pick an arbitrary uppercase
    constant in the module that starts with the specified prefix.
    This means, for example, that if called with a value of 1 and
    a prefix of "IFA_" it could return IFA_F_SECONDARY instead of
    IFA_ADDRESS. In the specific case of IFA_F_*, there is a
    workaround that ensures that these constants are never returned,
    but this does not fix the general problem.
    
    Fix this by allowing each module to explicitly specify a list of
    enum prefixes. _GetConstantName will pick the constant with the
    specified prefix.
    
    Also update iproute.py and sock_diag to use the new scheme, and
    add a unit test for it.
    
    Test: new unit test
    Change-Id: Ib3e2e0a5a44ea92203bef86873eb7cabc67630c7

--- net/test/iproute.py
+++ net/test/iproute.py
@@ -235,7 +235,7 @@ IFLA_VTI_REMOTE = 5
 
 CONSTANT_PREFIXES = netlink.MakeConstantPrefixes(
     ["RTM_", "RTN_", "RTPROT_", "RT_SCOPE_", "RT_TABLE_", "RTA_", "RTMGRP_",
-     "RTNLGRP_", "RTAX_", "IFA_", "IFA_F", "NDA_", "FRA_", "IFLA_",
+     "RTNLGRP_", "RTAX_", "IFA_", "IFA_F_", "NDA_", "FRA_", "IFLA_",
      "IFLA_INFO_", "IFLA_XFRM_", "IFLA_VTI_"])
 
 
