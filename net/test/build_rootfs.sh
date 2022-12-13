2b93c74056a54dd4fa90176cc22cf76b7b722bc3
Ying-Chun Liu (PaulLiu) <liupa@google.com>
2022-12-13 16:34:32 +0800

    net/test/build_rootfs.sh: remove usage of losetup on system partition
    losetup doesn't work in un-priviledge docker. Thus we remove
    the usage of losetup on system partition.
    
    Change-Id: I216afb71b9ec28975dbd8c258cfd644724abdbf8
    Signed-off-by: Ying-Chun Liu (PaulLiu) <liupa@google.com>

--- net/test/build_rootfs.sh
+++ net/test/build_rootfs.sh
@@ -384,10 +384,10 @@ if [[ -n "${system_partition}" ]]; then
   system_partition_num_sectors=$((${system_partition_end} - ${system_partition_start} + 1))
   system_partition_offset=$((${system_partition_start} * 512))
   system_partition_size=$((${system_partition_num_sectors} * 512))
-  loopdev_system="$(sudo losetup -f)"
-  sudo losetup --offset ${system_partition_offset} --sizelimit ${system_partition_size} "${loopdev_system}" "${disk}"
-  sudo fsck.vfat -a "${loopdev_system}" || true
-  sudo losetup -d "${loopdev_system}"
+  system_partition_tempfile=$(mktemp)
+  dd if="${disk}" of="${system_partition_tempfile}" bs=512 skip=${system_partition_start} count=${system_partition_num_sectors}
+  fsck.vfat -a "${system_partition_tempfile}" || true
+  dd if="${system_partition_tempfile}" of="${disk}" bs=512 seek=${system_partition_start} count=${system_partition_num_sectors} conv=fsync,notrunc
 fi
 
 # New workdir for the initrd extraction
