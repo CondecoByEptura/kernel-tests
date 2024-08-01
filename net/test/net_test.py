32ce0f4eb78211120bc969166758c3e945eb486a
h00013079 <huangdezhi@hihonor.com>
2024-08-01 15:41:47 +0800

    net_test: fix gsi detecting method
    "/metadata/gsi/dsu/booted" only exists on GSI booted via DSU,
    so switch to the leagcy way that used before to determine if it is a GSI build.
    
    https://android-review.googddlesource.com/c/platform/system/netd/+/2011276/3/tests/kernel_test.cpp
    
    Bug: 351955695
    Change-Id: Ic28c8a84a48a8006c4d0cebf5af8a654b46fc866
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>

--- net/test/net_test.py
+++ net/test/net_test.py
@@ -97,7 +97,7 @@ LINUX_VERSION = csocket.LinuxVersion()
 LINUX_ANY_VERSION = (0, 0)
 
 # From //system/gsid/libgsi.cpp IsGsiRunning()
-IS_GSI = os.access("/metadata/gsi/dsu/booted", os.F_OK)
+IS_GSI = os.access("/system/system_ext/etc/init/init.gsi.rc", os.F_OK)
 
 def KernelAtLeast(versions):
   """Checks the kernel version matches the specified versions.
